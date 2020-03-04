affdf99658db16bb5f676cef95f2e9a1cf6ee628
sphelele95 <sphelele@ikhokha.com>
2020-03-04 16:39:18 +0200

    - Updated server code

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const io = require("socket.io")(server);
 const port = 3000 | process.env.PORT;
 
 app.get('/', (req, res)=>{
-    //res.sendFile(__dirname + '/index.html');
-    res.send("Hello Socket Server")
+    res.send("Hello Socket Server");
 });
 
 
