56fb632cc7dc6c67b2cbf247eca74c1a711b47c0
sphelele95 <sphelele@ikhokha.com>
2020-03-05 11:31:54 +0200

    - Daily commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
-const port = 3000 | process.env.PORT;
+const port = 3000 || process.env.PORT;
 
 app.get('/', (req, res)=>{
     res.send("Hello Socket Server");
