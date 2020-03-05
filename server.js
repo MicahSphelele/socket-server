5c0163e99333b5a93fba3dccc6acc15ad2c072f3
sphelele95 <sphelele@ikhokha.com>
2020-03-05 14:42:32 +0200

    - Fixed PORT issue

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const io = require("socket.io")(server);
 const PORT = process.env.PORT ||  3000;
 
   server.listen(PORT,'0.0.0.0',()=>{
-    console.log(`Server is listening to port ${port}`);
+    console.log(`Server is listening to port ${PORT}`);
  });
  
 app.get('/', (req, res)=>{
